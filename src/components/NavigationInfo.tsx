dfdadd4179f32215830764998c4c8bab34a1c3af
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-04-26 23:08:40 -0600

    add-info-components

---
+++ src/components/NavigationInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavigationInfo = () => {
+  return (
+    <div>NavigationInfo</div>
+  )
+}
+
+export default NavigationInfo
---
+++ src/components/ProtectedRoutesInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProtectedRoutesInfo = () => {
+  return (
+    <div>ProtectedRoutesInfo</div>
+  )
+}
+
+export default ProtectedRoutesInfo
