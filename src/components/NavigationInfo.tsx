cef2f91bf594341bce3923641bec69a1b7a81832
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-04-27 01:04:28 -0600

    cambios

--- src/components/NavigationInfo.tsx
+++ src/components/NavigationInfo.tsx
@@ -58,9 +58,9 @@ export const Route = createRootRoute({
 
             <pre className="code-block-content">
               <code >
-{`import { createLazyFileRoute } from '@tanstack/react-router'
+{`import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createLazyFileRoute('/')({
+export const Route = createFileRoute('/')({
   component: Index,
 })
 
@@ -76,14 +76,14 @@ function Index() {
             </pre>
 
             <div className='guide-navigation-first-step-container-paths'>
-              <h3 className='text-guide-navigation'>src/routes/index.tsx</h3>
+              <h3 className='text-guide-navigation'>src/routes/about.tsx</h3>
             </div>
 
             <pre className="code-block-content">
               <code >
-{`import { createLazyFileRoute } from '@tanstack/react-router'
+{`import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createLazyFileRoute('/about')({
+export const Route = createFileRoute('/about')({
   component: About,
 })
 
@@ -93,13 +93,78 @@ function About() {
 `}
               </code>
             </pre>
+          </div>
 
-            
+          <div className='guide-navigation-steps'>
+              <h2 className='titles-guide-navigation'>Tercer paso</h2>
+              <p className='text-guide-navigation'>Finalmente, se modifica el siguiente archivo de tal manera que quede así.</p>
+
+              <div className='guide-navigation-first-step-container-paths'>
+                <h3 className='text-guide-navigation'>src/main.tsx</h3>
+                <h3 className='text-guide-navigation'>o en cualquer componente que se quiera renderizar, no necesariamente en el main.tsx .Pero en este primer ejemplo se hará en el main.tsx 
+                </h3>
+              </div>
+
+              <pre className="code-block-content">
+                <code > 
+{`
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
+`}
+                </code>
+              </pre>
           </div>
 
 
-      </div>
-|
+            <div className='guide-navigation-steps'>
+              <h2 className='titles-guide-navigation'>Cuarto paso</h2>
+              
+              <div className='guide-navigation-first-step-container-paths'>
+                <h3 className='text-guide-navigation'>Ejectura el comando</h3>
+              </div>
+
+              <pre className="code-block-content">
+              <code >
+{`  npm run dev
+`}
+              </code>
+            </pre>
+
+              <p>Una vez ejecutado el comando se creará el archivo </p>
+      
+              <div className='guide-navigation-first-step-container-paths'>
+                <h3 className='text-guide-navigation'>routeTree.gen.ts</h3>
+              </div>
+               <p>que no debes tocar, allí se generarán las rutas de la aplicación automáticamente, cada vez que crees un nuevo archivo dentro de <strong>__root.tsx</strong> y ejecutes el programa.</p>
+
+            </div>  
+          </div>
+
     </section>
   )
 }
