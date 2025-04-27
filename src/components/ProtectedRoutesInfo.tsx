b332f70f99d9f45cc942b3ea6516dcfaec7ee0ce
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-04-27 15:15:54 -0600

    carpeta-api

--- src/components/ProtectedRoutesInfo.tsx
+++ src/components/ProtectedRoutesInfo.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import './navigationInfo.css'
 const ProtectedRoutesInfo = () => {
   return (
-    <div>ProtectedRoutesInfo</div>
+    <div>
+      
+      <section className='Navigation-info'>
+
+        <div className='Navigation-info-container'>
+
+          <h1 className='titles-guide-navigation'>Guía de Protected Routes</h1>
+
+          <div className='guide-navigation-steps'>
+
+            <h2 className='titles-guide-navigation'>Primer paso</h2>
+            
+          </div>
+
+        </div>
+
+      </section>
+
+
+    </div>
+    
   )
 }
 
