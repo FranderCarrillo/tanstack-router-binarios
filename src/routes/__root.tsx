5a4e1554e159cef308938b3fac73cf5f07e252ac
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-04-28 15:14:12 -0600

    cambios

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,24 +6,31 @@ type RouterContext = {
   authentication: AuthContext;
 };
 
+const activeProps = {
+  style: {
+    fontWeight: "bold",
+    color: "black",
+  },
+};
+
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <div className="p-2 flex gap-2 navbar">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to="/" activeProps={activeProps} className="[&.active]:font-bold">
           Home
         </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to="/about" activeProps={activeProps} className="[&.active]:font-bold">
           About Us
         </Link>{' '}
-        <Link to="/contacUs" className="[&.active]:font-bold">
+        <Link to="/contacUs" activeProps={activeProps} className="[&.active]:font-bold">
           Contact Us
         </Link>{' '}
-        <Link to="/profile" className="[&.active]:font-bold">
+        <Link to="/profile" activeProps={activeProps} className="[&.active]:font-bold">
           Profile
         </Link>
         <div id='login'>
-          <Link to="/login" className="[&.active]:font-bold">
+          <Link to="/login" activeProps={activeProps} className="[&.active]:font-bold">
             Login
           </Link>
         </div>
