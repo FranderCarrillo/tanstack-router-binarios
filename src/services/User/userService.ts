550f212fd4a84c3b0708688228f6afb45d7051ef
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-04-28 11:35:20 -0600

    cambios

--- src/services/User/userService.ts
+++
@@ -1,8 +0,0 @@
-import { apiUrl } from "../../api/apiConfig";
-import { User } from "../../models/user";
-
-export async function getUserById(id: number): Promise<User>{
-    const response = await fetch(`${apiUrl}/users/${id}`);
-    const users = await response.json();
-    return users;
-}
